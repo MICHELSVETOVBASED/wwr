451a475dfebbb7bc0ce81e21ac5d98596697e7af
Schub Bergman <maks.kon.92@mail.ru>
2023-10-26 10:16:31 +0200

    RETURN TO THIS SHIIIT

--- wwr/Program.cs
+++ wwr/Program.cs
@@ -10,11 +10,14 @@ class Program
         int endValue = int.Parse(Console.ReadLine());
         //double[] arr2 = new double[endValue - startValue];
         PrintSum(Cubarize(startValue, endValue));
+        
+        
     }
-    static void Cubarize(int startValue, int endValue)
+    static int [] Cubarize(int startValue, int endValue)
     {
         int res = endValue - startValue;
         int[] arr = new int[res+1];
+        
         if (startValue < 0)
         {
             for (int i = 0, k = startValue; i != arr.Length; i++)
@@ -25,28 +28,37 @@ class Program
             for (int i = 0; i != arr.Length; i++)
             {
                 Console.WriteLine(arr[i]);
+            
             }
-            double[] arr1 = Array.ConvertAll(arr, i => (double)i);
-            for (int i = 0; i != arr.Length; i++)
-            {
-                arr1[i] = Math.Pow(arr[i], 3);
-                Console.WriteLine(arr1[i]);
-            }
+            
             
 
         }
-        var arr1 = Cubarize(arr1[0], arr1[arr1.Length - 1]);
+        double[] arr1 = Array.ConvertAll(arr, i => (double)i);
+        for (int i = 0; i != arr.Length; i++)
+        {
+            arr1[i] = Math.Pow(arr[i], 3);
+            Console.WriteLine(arr1[i]);
+        }
+        int[] arr2 = new int[res + 1];
+        for ( int i = 0; i!=arr.Length;i++)
+        {
+            arr2[i] = Convert.ToInt32(arr1[i]);
+        }
+        return arr2;
+   
+        
 
 
     }
-    public void PrintSum(double[] arr1)
+    public void PrintSum(int[] arr2)
     {
-        double sum = 0;
-        for (int i = 0; i != arr1.Length; i++)
+        int sum = 0;
+        for (int i = 0; i != arr2.Length; i++)
         {
-            sum += arr1[i];
+            sum += arr2[i];
         }
-        Console.WriteLine(sum); Console.WriteLine(sum); Console.WriteLine(sum); Console.WriteLine(sum);
+        Console.WriteLine(sum);
 
     }
 }
